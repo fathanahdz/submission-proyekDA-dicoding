dd915bbe21beb24f4d55635d67c22d46f88ee5ae
Nurunnisa Fathanah <dzsbnf21d@student.unhas.ac.id>
2024-03-03 23:45:43 +0800

    feat: Added wrangled and visualized data file

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+st.write(
+    """
+    # My first app
+    Hello, para calon praktisi data masa depan!
+    """
+)
